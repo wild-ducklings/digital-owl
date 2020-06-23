813f92334093f383f09504aa26160f353705bcb4
wild-marten <wildmarten00@gmail.com>
2020-06-24 00:06:02 +0200

    Poll view frontend

--- DigitalOwl.WebAppClient/src/App/App.tsx
+++ DigitalOwl.WebAppClient/src/App/App.tsx
@@ -4,8 +4,9 @@ import {makeStyles} from "@material-ui/styles";
 import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 import {LoginPage} from "../Page/Login/Login";
 import {Navbar} from "../Component/Navbar/Navbar";
-import {Sidebar} from "../Component/Sidebar/Sidebar"
+import {Sidebar} from "../Component/Sidebar/Sidebar";
 import {LandingPage} from "../Page/Landing/LandingPage";
+import {Poll} from "../Page/Poll/Poll";
 
 const UseStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -14,7 +15,7 @@ const UseStyles = makeStyles((theme: Theme) =>
             flexDirection: "column",
             background: theme.palette.background.default,
             justifyContent: "center",
-            display: "flex",
+            // display: "flex",
             alignItems: "center"
         },
         title: {
@@ -34,6 +35,9 @@ export default () => {
             <Switch>
                 <Route path={"/login"}>
                     <LoginPage/>
+                </Route>
+                <Route path={"/czygonciarzapojebalo"}>
+                    <Poll/>
                 </Route>
                 <Route path={"/"}>
                     <LandingPage/>
